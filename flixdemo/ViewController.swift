c316907e04bed91a501cef7b6f811080c44f1626
HarukaOshikawa <haruka.oshikawa@stonybrook.edu>
2021-02-19 16:49:15 -0500

    Initial Commit

---
+++ flixdemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixdemo
+//
+//  Created by Haruka Oshikawa on 2/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
