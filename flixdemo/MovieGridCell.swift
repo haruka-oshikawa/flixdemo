e484419ffb9061bfea704c756204998e139deb65
HarukaOshikawa <haruka.oshikawa@stonybrook.edu>
2021-02-26 23:02:17 -0500

    Assignment #2

---
+++ flixdemo/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixdemo
+//
+//  Created by Haruka Oshikawa on 2/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
